41a6bb622e649f876811d3e20e8d5afcb2df90aa
Jonathan Decker <Discord@hotmail.de>
2024-11-16 21:16:11 +0100

    Expanded melvonaut.

--- src/melvonaut/__main__.py
+++ src/melvonaut/__main__.py
@@ -1,18 +1,20 @@
 """Command-line interface."""
 
 import asyncio
+import datetime
+import json
 import signal
 import sys
 from collections.abc import Callable
+from enum import StrEnum
 from pprint import pprint
-from typing import Any, Awaitable
+from typing import Any, Awaitable, Optional
 
 import aiohttp
 import click
+from aiofile import async_open
 from loguru import logger
 import uvloop
-import enum
-
 from pydantic import BaseModel, ConfigDict
 
 import melvonaut.constants as con
@@ -27,36 +29,129 @@ logger.add(
     diagnose=True,
 )
 
+loop = uvloop.new_event_loop()
 
-class State(enum.Enum):
-    Deployment = "Deployment"
-    Acquisition = "Acquisition"
-    Charge = "Charge"
-    Safe = "Safe"
-    Comm = "Comm"
-    Transition = "Transition"
-    Unknown = "Unknown"
+
+class State(StrEnum):
+    Deployment = "deployment"
+    Acquisition = "acquisition"
+    Charge = "charge"
+    Safe = "safe"
+    Communication = "communication"
+    Transition = "transition"
+    Unknown = "none"
+
+
+class Angle(StrEnum):
+    Wide = "wide"
+    Narrow = "narrow"
+    Normal = "normal"
+    Unknown = "unknown"
+
+
+class AreaCovered(BaseModel):
+    narrow: float
+    normal: float
+    wide: float
+
+class DataVolume(BaseModel):
+    data_volume_received: int
+    data_volume_sent: int
+
+
+class Telemetry(BaseModel):
+    model_config = ConfigDict(use_enum_values=True)
+
+    active_time: float
+    angle: Angle
+    area_covered: AreaCovered
+    battery: float
+    data_volume: DataVolume
+    distance_covered: float
+    fuel: float
+    height_y: float
+    images_taken: int
+    max_battery: float
+    objectives_done: int
+    objectives_points: int
+    simulation_speed: int
+    state: State
+    timestamp: datetime.datetime
+    vx: float
+    vy: float
+    width_x: float
+
+    async def store_observation(self) -> None:
+        logger.info("Storing observation")
+        try:
+            async with async_open(con.TELEMETRY_LOCATION, "r") as afp:
+                raw_telemetry = await afp.read()
+                dict_telemetry = json.loads(raw_telemetry)
+        except FileNotFoundError:
+            logger.debug(f"{con.TELEMETRY_LOCATION} does not exist.")
+            dict_telemetry = {}
+
+        timestamp = self.timestamp.isoformat()
+        new_telemetry_entry = self.model_dump(exclude={"timestamp"})
+        dict_telemetry[timestamp] = new_telemetry_entry
+        json_telemetry = json.dumps(dict_telemetry, indent=4, sort_keys=True)
+
+        async with async_open(con.TELEMETRY_LOCATION, "w") as afp:
+            logger.debug(f"Writing to {con.TELEMETRY_LOCATION}")
+            await afp.write(str(json_telemetry))
+        logger.debug("Observation stored")
+
+    def model_post_init(self, __context__: Any) -> None:
+        loop.create_task(self.store_observation())
+
+
+current_telemetry = None
 
 
 class Timer(BaseModel):
     model_config = ConfigDict(arbitrary_types_allowed=True)
 
-    timeout: int
-    callback: Callable[[], Awaitable[Any]]
-    task: asyncio.Task[None]
+    _timeout: int
+    _callback: Callable[[], Awaitable[Any]]
+    _task: asyncio.Task[None]
 
-    def __init__(self, timeout: int, callback: Callable[[], Awaitable[Any]]) -> None:
+    def __init__(self, timeout: int, callback: Callable[[], Awaitable[Any]]):
         super().__init__()
-        self.timeout = timeout
-        self.callback = callback
-        self.task = asyncio.create_task(self._job())
+        self._timeout = timeout
+        self._callback = callback
+        self._task = asyncio.create_task(self._job())
 
     async def _job(self) -> None:
-        await asyncio.sleep(self.timeout)
-        await self.callback()
+        await asyncio.sleep(self._timeout)
+        await self._callback()
 
     def cancel(self) -> None:
-        self.task.cancel()
+        self._task.cancel()
+
+    def get_task(self) -> asyncio.Task[None]:
+        return self._task
+
+
+class StatePlanner(BaseModel):
+    current_telemetry: Optional[Telemetry] = None
+    previous_telemetry: Optional[Telemetry] = None
+
+    def get_current_state(self) -> State:
+        return self.current_telemetry.state
+
+    def get_previous_state(self) -> State:
+        return self.previous_telemetry.state
+
+    def update_telemetry(self, new_telemetry: Telemetry) -> None:
+        self.previous_telemetry = self.current_telemetry
+        self.current_telemetry = new_telemetry
+
+        if self.previous_telemetry is not None:
+            pass
+
+
+
+state_planner = StatePlanner()
 
 
 async def get_observations() -> None:
@@ -66,6 +161,7 @@ async def get_observations() -> None:
                 json_response = await response.json()
                 logger.info("Received observations")
                 pprint(json_response, indent=4, sort_dicts=True)
+                state_planner.update_telemetry(Telemetry(**json_response))
             else:
                 logger.warning(f"Failed to get observations: {response.status}")
 
@@ -74,7 +170,7 @@ async def run_get_observations() -> None:
     await get_observations()
     while True:
         logger.info("Submitted observations request")
-        observe_task = Timer(10, get_observations).task
+        observe_task = Timer(timeout=con.OBSERVATION_REFRESH_RATE, callback=get_observations).get_task()
         await asyncio.gather(observe_task)
 
 
@@ -106,7 +202,6 @@ def cancel_tasks() -> None:
 
 
 def start_event_loop() -> None:
-    loop = uvloop.new_event_loop()
     for sig in (signal.SIGINT, signal.SIGTERM):
         loop.add_signal_handler(sig, cancel_tasks)
 
--- src/melvonaut/constants.py
+++ src/melvonaut/constants.py
@@ -4,7 +4,8 @@ System constant values
 :author: Jonathan Decker
 """
 
-LOG_LOCATION = "logs/melvonaut/melvonaut_{time:YYYY-MM-DD_HH}.log"
+LOG_LOCATION = "logs/melvonaut/log_melvonaut_{time:YYYY-MM-DD_HH}.log"
+TELEMETRY_LOCATION = "logs/melvonaut/telemetry_melvonaut.json"
 
 BASE_URL = "http://10.100.10.11:33000/"
 
@@ -14,3 +15,5 @@ OBSERVATION_ENDPOINT = f"{BASE_URL}observation"
 CONTROL_ENDPOINT = f"{BASE_URL}control"
 IMAGE_ENDPOINT = f"{BASE_URL}image"
 BEACON_ENDPOINT = f"{BASE_URL}beacon"
+
+OBSERVATION_REFRESH_RATE = 3 # Seconds between observation requests
