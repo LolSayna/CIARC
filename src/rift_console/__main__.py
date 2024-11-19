ce60357ebd270fea999e9a4c0806c1688d157f8d
Mareike Burg <mareike.burg@web.de>
2024-11-19 16:36:31 +0100

    first console prototyp with html :)

--- src/rift_console/__main__.py
+++ src/rift_console/__main__.py
@@ -1,15 +1,50 @@
 """Command-line interface."""
 
 import click
-from flask import Flask
+import datetime
+from enum import StrEnum
+from flask import Flask, render_template
+
+class State(StrEnum):
+    Deployment = "deployment"
+    Acquisition = "acquisition"
+    Charge = "charge"
+    Safe = "safe"
+    Communication = "communication"
+    Transition = "transition"
+    Unknown = "none"
+
+class Melvin():
+    active_time: float
+    battery: float
+    distance_covered: float
+    fuel: float
+    height_y: float
+    images_taken: int
+    max_battery: float
+    objectives_done: int
+    objectives_points: int
+    simulation_speed: int
+    state: State
+    timestamp: datetime.datetime
+    vx: float
+    vy: float
+    width_x: float
 
 app = Flask(__name__)
 
-
 @app.route("/")
+def index() -> str:
+    """index"""
+    return render_template('console.html', energy=50, fuel=75)
+
+
+@app.route("/hello")
 def hello_world() -> str:
     """Simple Hello World endpoint."""
     return "Hello, World from Rift Console's Flask Server!"
+
+
 
 
 @click.group()
@@ -23,7 +58,7 @@ def main() -> None:
 def run_server() -> None:
     """Run the Flask development server on port 8000."""
     click.echo("Starting Flask development server on port 8000...")
-    app.run(port=8000, debug=False)
+    app.run(port=8000, debug=True)
 
 
 @main.command()
