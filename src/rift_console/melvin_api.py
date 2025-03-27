a294d022da3a8a703e85fdf07f5f63ab3482e1fc
Mareike Burg <mareike.burg@web.de>
2025-03-27 14:00:16 +0100

    Still issue with tunnel

--- src/rift_console/melvin_api.py
+++ src/rift_console/melvin_api.py
@@ -19,14 +19,14 @@ port = "8080"
 
 def create_tunnel() -> None:
     # cmd = "ssh melvin -N -L 8080:localhost:8080 -o ConnectTimeout=1s"
-    cmd = "sshpass -f CIARC/.ssh-pw ssh melvin -N -L 8080:localhost:8080 -o ConnectTimeout=1s"
+    cmd = "sshpass -f CIARC/.ssh-pw ssh root@10.100.50.1 -N -L 8080:localhost:8080 -o ConnectTimeout=1s"
     timeout = 60 * 15  #kill connection after 15 min
 
     process = subprocess.Popen(cmd, shell=True, preexec_fn=os.setsid)
-
+    logger.info(f"Started tunnel: {process.pid}")
     # Function to terminate the process
     def terminate_process():
-        print("Terminating the process...")
+        logger.warning("Cleanup tunnel")
         os.killpg(os.getpgid(process.pid), signal.SIGTERM)
 
     # Start a timer to terminate the process after timeout
