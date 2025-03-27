9ae29659d12356a70c487fcf35a611e2a6929526
Mareike Burg <mareike.burg@web.de>
2025-03-27 11:49:36 +0100

    more

--- src/melvonaut/__main__.py
+++ src/melvonaut/__main__.py
@@ -173,7 +173,7 @@ async def get_announcements2(last_id: Optional[str] = None) -> Optional[str]:
 
                             current_event = Event(
                                 event=line_content,
-                                id=line_id,
+                                id=line_id or 0,
                                 timestamp=timestamp,
                                 current_x=current_x,
                                 current_y=current_y,
